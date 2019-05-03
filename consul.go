a6af0dd0f5aa41bfecf224cb0b3e76e656826219
orvice <orvice@gmail.com>
2019-05-03 12:25:22 +0800

    add consul config

---
+++ consul.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"git.orx.me/cat/contract"
+	"github.com/hashicorp/consul/api"
+	"github.com/orvice/kit/consul"
+)
+
+type ConsulConfig struct {
+	client *consul.Client
+}
+
+var _ contract.Config = new(ConsulConfig)
+
+func (c *ConsulConfig) Get(key string) ([]byte, error) {
+	return c.client.KVGet(key)
+}
+
+func (c *ConsulConfig) Set(key string, value []byte) error {
+	return c.client.KVSet(key, value)
+}
+
+func NewConsulConfig(host, token string) (*ConsulConfig, error) {
+
+	client, err := consul.NewClient(&api.Config{
+		Address: host,
+		Token:   token,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &ConsulConfig{
+		client: client,
+	}, nil
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module git.orx.me/cat/config
+
+go 1.12
+
+require (
+	git.orx.me/cat/contract v0.0.0-20190503041632-e76718d423b9
+	github.com/hashicorp/consul/api v1.0.1 // indirect
+	github.com/orvice/kit v0.0.0-20190224124836-1d905413b870
+)
